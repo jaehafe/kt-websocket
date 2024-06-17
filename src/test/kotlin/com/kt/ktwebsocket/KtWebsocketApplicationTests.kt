431a62b8110799ddf6ee10726c98da9dd8b374b7
jaehafe <wogkdkrm112@gmail.com>
2024-06-17 20:59:32 +0900

    init: kotlin web socket

---
+++ src/test/kotlin/com/kt/ktwebsocket/KtWebsocketApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.kt.ktwebsocket
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class KtWebsocketApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
